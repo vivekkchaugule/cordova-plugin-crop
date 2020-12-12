59e41c268d0a6161a579c895038102dc789e8fef
Vivek Chaugule <49585527+vivekkchaugule@users.noreply.github.com>
2020-12-12 18:59:54 +0530

    Update CropPlugin.java
    Fix Directory does not get created on Android External Storage

--- src/android/CropPlugin.java
+++ src/android/CropPlugin.java
@@ -88,16 +88,8 @@ public class CropPlugin extends CordovaPlugin {
 
     private String getTempDirectoryPath() {
         File cache = null;
-
-        // SD Card Mounted
-        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-            cache = new File(Environment.getExternalStorageDirectory().getAbsolutePath() +
-                    "/Android/data/" + cordova.getActivity().getPackageName() + "/cache/");
-        }
-        // Use internal storage
-        else {
-            cache = cordova.getActivity().getCacheDir();
-        }
+        
+        cache = cordova.getActivity().getCacheDir();
 
         // Create the cache directory if it doesn't exist
         cache.mkdirs();
